a79fa6c74370f9e8241aedb0be353799b0716806
LupeF <46634202+LupeF@users.noreply.github.com>
2022-07-07 11:14:40 -0700

    will add utilities partials and event listner

--- js/app.js
+++ js/app.js
@@ -1,7 +1,35 @@
-const noteDiv = document.getElementById('note');
+const noteDiv = document.getElementById('note-div');
 
 document.getElementById('note-button').addEventListener ('click', (e)=> {
-    if(){
-
-    }
-})
+    noteDiv.innerHTML = 
+    `<div class="note-container">
+                <h1> Create Note </h1>
+                <p> Choose a Label </p>
+                <div class="color-container">
+                    <div class="color-1"></div>
+                    <div class="color-2"></div>
+                    <div class="color-3"></div>
+                    <div class="color-4"></div>
+                    <div class="color-5"></div>
+                    <div class="color-6"></div>
+                    <div class="color-7"></div>
+                    <div class="color-8"></div>
+                    <div class="color-9"></div>
+                    <div class="color-10"></div>
+                    <div class="color-11"></div>
+                    <div class="color-12"></div>
+                    <div class="color-13"></div>
+                    <div class="color-14"></div>
+                    <div class="color-15"></div>
+                    <div class="color-16"></div>
+                    <div class="color-17"></div>
+                    <div class="color-18"></div>
+                </div>
+                <p> Give your Note a title </p>
+                <input></input>
+                <p>Write down your note</p>
+                <textarea></textarea>
+                <button>Post Note</button>
+                
+    </div>`;
+});
