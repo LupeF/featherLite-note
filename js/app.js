91a4b03d95ffec466462473a1156f7bc60750cdf
LupeF <46634202+LupeF@users.noreply.github.com>
2022-08-01 07:48:37 -0700

    fix issues deleting form values

--- js/app.js
+++ js/app.js
@@ -9,34 +9,34 @@ document.getElementById('note-button').addEventListener ('click', (e)=> {
         noteDiv.innerHTML = 
         `<div id="container">
             <div class="note-container">
-                    <h1> Create Note </h1>
-                    <p> Choose a Label </p>
-                    <div class="color-container">
-                        <div class="color-1"></div>
-                        <div class="color-2"></div>
-                        <div class="color-3"></div>
-                        <div class="color-4"></div>
-                        <div class="color-5"></div>
-                        <div class="color-6"></div>
-                        <div class="color-7"></div>
-                        <div class="color-8"></div>
-                        <div class="color-9"></div>
-                        <div class="color-10"></div>
-                        <div class="color-11"></div>
-                        <div class="color-12"></div>
-                        <div class="color-13"></div>
-                        <div class="color-14"></div>
-                        <div class="color-15"></div>
-                        <div class="color-16"></div>
-                        <div class="color-17"></div>
-                        <div class="color-18"></div>
-                    </div>
-                    <p> Give your Note a title </p>
-                    <input id="title-Heading"></input>
-                    <p>Write down your note</p>
-                    <textarea id="note-value"></textarea>
-                    <button id="post">Post Note</button>
-                    <button id="cancel">Cancel</button>
+                <h1> Create Note </h1>
+                <p> Choose a Label </p>
+                <div class="color-container">
+                    <div class="color-1"></div>
+                    <div class="color-2"></div>
+                    <div class="color-3"></div>
+                    <div class="color-4"></div>
+                    <div class="color-5"></div>
+                    <div class="color-6"></div>
+                    <div class="color-7"></div>
+                    <div class="color-8"></div>
+                    <div class="color-9"></div>
+                    <div class="color-10"></div>
+                    <div class="color-11"></div>
+                    <div class="color-12"></div>
+                    <div class="color-13"></div>
+                    <div class="color-14"></div>
+                    <div class="color-15"></div>
+                    <div class="color-16"></div>
+                    <div class="color-17"></div>
+                    <div class="color-18"></div>
+                </div>
+                <p> Give your Note a title </p>
+                <input id="title-Heading"></input>
+                <p>Write down your note</p>
+                <textarea id="note-value"></textarea>
+                <button id="post">Post Note</button>
+                <button id="cancel">Cancel</button>
             </div>
         </div>`;
     }
@@ -49,7 +49,7 @@ document.getElementById('note-button').addEventListener ('click', (e)=> {
 //********************************/
 // Appends note to the display
 //********************************/
-    const title = document.getElementById('title-Heading')
+    const title = document.getElementById('title-Heading');
     const note = document.getElementById('note-value');
     document.getElementById('post').addEventListener('click',()=>{
         const section = document.createElement('section');
@@ -66,9 +66,9 @@ document.getElementById('note-button').addEventListener ('click', (e)=> {
         `;
         document.querySelector('.grid').prepend(section);
         noteDiv.style.display = 'none';
-        if(noteDiv.style.display === 'none'){  
-            h1.textContent = ""; //erases Content
-            p.textContent = "";  //erases Content 
+        if(noteDiv.style.display === 'none'){ 
+           title.value = ""; // Erases Content
+            note.value = "";  //Erases Content 
         }
     });
 });
